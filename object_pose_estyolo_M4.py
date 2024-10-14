7d3563e2da0072f40efa3e93764c2e443a778f8b
CherylSW <cooi0006@student.monash.edu>
2024-10-14 15:37:52 +0800

    m5 newnew

--- object_pose_estyolo_M4.py
+++ object_pose_estyolo_M4.py
@@ -41,7 +41,7 @@ def estimate_pose(camera_matrix, obj_info, robot_pose):
 
 
     # seems to be the best for now considering how scuffed our caps bounding box is 
-    object_dimensions_dict = {'1': [0.074,0.074,0.087], '2': [0.081, 0.081, 0.074], 
+    object_dimensions_dict = {'1': [0.074,0.074,0.087], '2': [0.081, 0.081, 0.072], 
                               '3': [0.075, 0.075, 0.075], '4': [0.113, 0.067, 0.056], 
                               '5': [0.074, 0.074, 0.102]}
     
